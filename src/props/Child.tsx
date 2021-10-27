24e27d636e9f4d20cdf1639a2df91ebade4b3767
Jay Hernandez <jeric614@yahoo.com>
2021-10-27 09:51:31 +0900

    function w/interface

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,8 +1,34 @@
 //define what props expect to receive from parent
 interface ChildProps {
   color: string;
+  onClick: () => void;
 }
 
-export const Child = ({ color }: ChildProps) => {
-  return <div>{color}</div>;
+export const Child = ({ color, onClick }: ChildProps) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}></button>
+    </div>
+  );
 };
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color, onClick }) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}></button>
+    </div>
+  );
+};
+
+// use fc shortcut to be op
+// interface Props {
+
+// }
+
+// const : FunctionComponent<Props> = () => {
+//   return (  );
+// }
+
+// export default ;
